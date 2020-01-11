76a687e4f478fc58dcdf758c73cdff49d90ef543
mayank dixit <44468368+mayank0802@users.noreply.github.com>
2020-01-11 14:14:06 +0530

    Update settings.py

--- codemon-master/sih_django/libauto/libauto/settings.py
+++ codemon-master/sih_django/libauto/libauto/settings.py
@@ -32,13 +32,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'stport.apps.StportConfig',
+    'libadmin.apps.LibadminConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'libadmin'
+    
 ]
 
 MIDDLEWARE = [
