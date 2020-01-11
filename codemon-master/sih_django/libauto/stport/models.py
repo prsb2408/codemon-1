d934f8f38ef60c97ba7187aa45d6c278de9ad3f2
mayank dixit <44468368+mayank0802@users.noreply.github.com>
2020-01-11 13:59:49 +0530

    Update models.py

--- codemon-master/sih_django/libauto/stport/models.py
+++ codemon-master/sih_django/libauto/stport/models.py
@@ -7,6 +7,7 @@ class student_info(models.Model):
 	student_phone = models.IntegerField()
 	student_email = models.CharField(max_length = 50)
 	student_password = models.CharField(max_length = 60)
-	student_birth = models.DateTimeField('birth_date')
 	student_city = models.CharField(max_length = 20)
-	student_street = models.CharField(max_length = 20) 
+	student_street = models.CharField(max_length = 20)
+	def __str__(self): 
+		return self.student_name
